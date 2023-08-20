5795478932b8df4b6b8b9652304dd663f0a33056
Max <max.teiger@gmail.com>
2023-08-20 21:15:02 +0200

    feat(completion): Exercises 03 ok

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(stringToBeReversed) {
+	stringReversed = "";
+	for(let i = stringToBeReversed.length-1; i >= 0; i--){
+		stringReversed += stringToBeReversed[i];
+	}
+	return stringReversed;
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
+
+console.log(reverseString("Hello!"));
