ec128de112e69c10897dc19bf2c014d551c53af7
Max <max.teiger@gmail.com>
2023-08-21 19:18:03 +0200

    feat(04): complete exercise remove from Array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function(arrayToModify, elementToRemove) {
-	const index = arrayToModify.indexOf(elementToRemove);
-	return arrayToModify.splice(index, 1);
+const removeFromArray = function(arrayToModify, ...args) {
+	arrayToReturn = [];
+	
+	arrayToModify.forEach(element => {
+		console.log(args.indexOf(element));
+		if(args.indexOf(element) === -1){
+			arrayToReturn.push(element);
+			console.log("Element: " + element + " | Array to return: " + arrayToReturn + " | Array to modify: " + arrayToModify);
+		}
+	});
+
+	return arrayToReturn;
 };
 
 // Do not edit below this line
