575398245a5fd93133a5dfa3a280fd8c05ebf509
Max <max.teiger@gmail.com>
2023-08-21 22:29:31 +0200

    feat(06): leap year

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,17 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+	if(!Number.isInteger(year)) return "ERROR";
+	if(year%4 === 0) {
+		if(year%100 === 0) {
+			if(year%400 === 0){
+				return true;
+			}
+			return false;
+		}
+		return true;
+	}
+	return false;
 };
 
 // Do not edit below this line
 module.exports = leapYears;
+console.log(leapYears(700));
