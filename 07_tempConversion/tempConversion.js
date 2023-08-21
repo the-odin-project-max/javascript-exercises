60abf166d34794d73b80e366a8406903cf9cae17
Max <max.teiger@gmail.com>
2023-08-21 22:42:21 +0200

    feat(07): temperature

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,10 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(x) {
+	return Math.round(((x-32)*5/9)*10)/10;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(x) {
+	return Math.round(((9/5*x)+32)*10)/10;
 };
 
 // Do not edit below this line
@@ -9,3 +12,5 @@ module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
+
+console.log(convertToFahrenheit(0));
