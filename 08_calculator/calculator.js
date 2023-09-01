e1eb02cee1e11c507824eb651615a68d06092dab
Max <max.teiger@gmail.com>
2023-09-01 16:55:42 +0200

    feat(calculator): complete calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,26 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	return numbers.reduce((acc, cur) => acc + cur, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+	return numbers.reduce((acc, cur) => acc * cur, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	if(number === 0) return 1;
+	return number * factorial(number - 1);
 };
 
 // Do not edit below this line
