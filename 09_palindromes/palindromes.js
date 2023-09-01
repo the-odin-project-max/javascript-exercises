4745579214f7d6563c3145ceeaf7f4e14d627137
Max <max.teiger@gmail.com>
2023-09-01 19:19:57 +0200

    feat(palindromes): complete exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+	string = string.toLowerCase().replace(/[^A-Za-z0-9]/g, "");
+	return string.split("").reverse().join("") === string;
 };
+
+console.log(palindromes("r3ace3car"))
 
 // Do not edit below this line
 module.exports = palindromes;
