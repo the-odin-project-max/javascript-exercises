1f993ce987d20c9d01c16423f485e3315c5ee159
Max <max.teiger@gmail.com>
2023-09-03 16:08:52 +0200

    feat(findtheoldest): complete exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,30 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+	return people.reduce((oldest, person) => {
+		if (!("yearOfDeath" in person)) person.yearOfDeath = new Date().getFullYear();
+		if (!("yearOfDeath" in oldest)) oldest.yearOfDeath = new Date().getFullYear();
+		const oldestAge = oldest.yearOfDeath - oldest.yearOfBirth;
+		const personAge = person.yearOfDeath - person.yearOfBirth;
+		return oldestAge < personAge ? person : oldest;
+	});
 };
 
+const people = [
+	{
+	  name: "Carly",
+	  yearOfBirth: 2018,
+	},
+	{
+	  name: "Ray",
+	  yearOfBirth: 1962,
+	  yearOfDeath: 2011,
+	},
+	{
+	  name: "Jane",
+	  yearOfBirth: 1912,
+	  yearOfDeath: 1941,
+	},
+  ]
+
+  console.log(findTheOldest(people));
 // Do not edit below this line
 module.exports = findTheOldest;
