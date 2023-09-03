579fc49a5e6a86a6abf2dd72e67ae1dd8515b5bf
Max <max.teiger@gmail.com>
2023-09-03 15:50:44 +0200

    feat(fibonacci): complete exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function (n) {
+	let fib = 0;
+	let fib1 = 1;
+	let fib2 = 0;
+	if (n < 0) {
+		return "OOPS";
+	}
+	for (let i = 0; i < n; i++) {
+		fib = fib1 + fib2;
+		fib2 = fib1;
+		fib1 = fib;
+	}
+	return fib2;
 };
+
+console.log(fibonacci(4));
 
 // Do not edit below this line
 module.exports = fibonacci;
